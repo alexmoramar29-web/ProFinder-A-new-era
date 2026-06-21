05af64cff02c0447089add3a33a453c63b670aed
alexmoramar29-web <alexmoramar29@gmail.com>
2026-06-21 10:13:46 -0600

    Agregue lo de publicar servicio(fixeado 4)

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -1,7 +1,6 @@
 import { ScrollViewStyleReset } from 'expo-router/html';
 import React from 'react';
 
-// Este archivo SOLAMENTE se ejecuta cuando alguien abre tu app en la web (como en Render)
 export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
@@ -9,19 +8,42 @@ export default function Root({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         
-        {/* CANDADO 1: La regla estricta para el navegador */}
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        {/* Candado Nivel 1: La regla básica */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         
         <ScrollViewStyleReset />
         
-        {/* CANDADO 2: La magia oscura (CSS). 
-            touch-action: pan-x pan-y -> Solo permite deslizar arriba/abajo e izquierda/derecha.
-            Bloquea por completo el gesto de pellizcar para hacer zoom. */}
+        {/* Candado Nivel 2: CSS */}
         <style dangerouslySetInnerHTML={{ __html: `
           body {
             touch-action: pan-x pan-y;
             overscroll-behavior: none;
           }
+        ` }} />
+
+        {/* LA BOMBA NUCLEAR: Candado Nivel 3 (JavaScript) */}
+        <script dangerouslySetInnerHTML={{ __html: `
+        1. Bloquea el gesto de "pellizcar" exclusivo de los iPhone (Safari)
+          document.addEventListener('gesturestart', function (e) {
+            e.preventDefault();
+          });
+
+          // 2. Bloquea el uso de 2 o más dedos al mismo tiempo en cualquier celular (Android/iOS)
+          document.addEventListener('touchmove', function(e) {
+            if (e.touches.length > 1) {
+              e.preventDefault();
+            }
+          }, { passive: false });
+
+          // 3. Bloquea el molesto "Doble Toque" rápido que también hace zoom
+          let lastTouchEnd = 0;
+          document.addEventListener('touchend', function (e) {
+            let now = (new Date()).getTime();
+            if (now - lastTouchEnd <= 300) {
+              e.preventDefault();
+            }
+            lastTouchEnd = now;
+          }, false);
         ` }} />
       </head>
       <body>{children}</body>
